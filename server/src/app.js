5108d35360f5acacd02637cef71a118b30c35230
unknown <38fight.name>
2020-09-16 23:10:32 +0200

    Update app.js
    :fire: Test

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const morgan = require('morgan');
 const helmet = require('helmet');
+const morgan = require('morgan');
 const cors = require('cors');
 const routes = require('./routes');
 require('dotenv').config();
